c9144613dba598c908d28e9106c833a267d815af
kevinjmurray <kjmurray1@loyola.edu>
2023-05-09 17:29:50 -0400

    Revert "Whitenoise added"
    This reverts commit 588e37175c85aace8fc926cca5395cc64a526b31.

--- mde/settings.py
+++ mde/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
-    'main',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -132,10 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
