2f53474aaf3b5d555114cfae81ae8229a73473c0
kevinjmurray <kjmurray1@loyola.edu>
2023-05-09 17:23:43 -0400

    static files collected

--- mde/settings.py
+++ mde/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import dotenv
+#import dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
